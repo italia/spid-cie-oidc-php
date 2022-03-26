dc95d7a6dc900b54e60d119ca0037d072ba96bc1
damikael <michele.damico@linfaservice.it>
2022-03-26 11:56:19 +0100

    upd test

--- www/test.php
+++ www/test.php
@@ -1,5 +1,17 @@
 <?php
 
-foreach ($_POST as $k => $v) {
-    echo "<p>" . $k . ": <b>" . $v . "</b></p>";
+if(isset($_POST) && $_SERVER['HTTP_ORIGIN']=='https://'.$_SERVER['HTTP_HOST']) {
+
+    echo "<div>";
+    foreach ($_POST as $k => $v) {
+        echo "<p>" . $k . ": <b>" . $v . "</b></p>";
+    }
+
+    echo "<div><a href=\"oidc/rp/logout\">Esci</a></div>";
+    echo "</div>";
+
+} else {
+    header("Location: oidc/rp/authz");
 }
+
+?>
