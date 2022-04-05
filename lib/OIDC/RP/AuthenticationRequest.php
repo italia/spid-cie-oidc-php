e8e8d9f5464cb24071a8b244b8bcb64facb1ccde
damikael <michele.damico@linfaservice.it>
2022-04-05 12:57:01 +0200

    fix: authentication request redirect uri

--- lib/OIDC/RP/AuthenticationRequest.php
+++ lib/OIDC/RP/AuthenticationRequest.php
@@ -65,7 +65,7 @@ class AuthenticationRequest
     {
         $client_id = $this->config->client_id;
         $redirect_uri = Util::stringEndsWith($client_id, '/') ? $client_id : $client_id . '/';
-        if($this->config->service_name != '') {
+        if ($this->config->service_name != '') {
             $redirect_uri .= $this->config->service_name . '/';
         }
         $redirect_uri .= 'oidc/rp/redirect';
