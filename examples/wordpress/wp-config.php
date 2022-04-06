08658554842035db04f80e1ff64ed4ef952a4a50
damikael <michele.damico@linfaservice.it>
2022-04-06 19:51:47 +0200

    ci: fix wordpress example config

--- examples/wordpress/wp-config.php
+++ examples/wordpress/wp-config.php
@@ -92,8 +92,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
-define('WP_SITEURL', '//' . $_SERVER['HTTP_HOST'] );
-define('WP_HOME', '//' . $_SERVER['HTTP_HOST']);
-
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
