f68b308cdcafcd93e54d619cd110940fcaa9eae1
damikael <michele.damico@linfaservice.it>
2022-04-09 22:51:14 +0200

    ci: upd tests

---
+++ tests/UtilTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use PHPUnit\Framework\TestCase as TestCase;
+use SPID_CIE_OIDC_PHP\Core\Util;
+
+/**
+ * @covers SPID_CIE_OIDC_PHP\Core\Util
+ */
+class UtilTest extends TestCase
+{
+    /**
+     * @covers SPID_CIE_OIDC_PHP\Core\Util
+     */
+    public function test_Util()
+    {
+        $code = Util::getRandomCode();
+        $this->assertTrue(strlen($code)==64);
+
+        $code_verifier = "code";
+        $code_challenge = Util::getCodeChallenge($code_verifier);
+        $mustBe = Util::base64UrlEncode(hash('sha256', $code_verifier, true));
+        $this->assertEquals($code_challenge, $mustBe);
+
+        $string = "string";
+        $encoded = Util::base64UrlEncode($string);
+        $decoded = Util::base64UrlDecode($encoded);
+        $this->assertEquals($string, $decoded);
+
+        $startsWith = Util::stringStartsWith($string, "s");
+        $this->assertTrue($startsWith);
+
+        $endsWith = Util::stringEndsWith($string, "g");
+        $this->assertTrue($endsWith);
+    }
+}
