c8f9155c3a21c9151e23d81b95e06d65278d34c2
rglauco <37829079+rglauco@users.noreply.github.com>
2023-05-19 22:04:38 +0200

    fix: amended Dockerfile for db and config inclusion and corrected WP version compared to DB version

--- examples/wordpress/wp-config.php
+++ examples/wordpress/wp-config.php
@@ -37,6 +37,7 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
 /**#@+
  * Authentication unique keys and salts.
  *
