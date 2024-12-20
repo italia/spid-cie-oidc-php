6dd2b53b704bfd1f7082e68c90ae568c17767658
unicg <u@ni.cg>
2024-12-20 22:11:30 +0100

    Fix fiscal number claim.

--- lib/OIDC/OP/UserinfoEndpoint.php
+++ lib/OIDC/OP/UserinfoEndpoint.php
@@ -62,7 +62,8 @@ class UserinfoEndpoint
             }
             $this->database->log("UserinfoEndpoint", "USERINFO", "Bearer: " . $bearer);
             $userinfo = (array) $this->database->getUserinfo($bearer);
-            $userinfo['sub'] = $userinfo['https://attributes_eid_gov_it/fiscal_number'];
+            $fiscalNumber = isset($userinfo['https://attributes_eid_gov_it/fiscal_number'])? $userinfo['https://attributes_eid_gov_it/fiscal_number'] : $userinfo['fiscal_number'];
+            $userinfo['sub'] = $fiscalNumber;
             $this->database->log("UserinfoEndpoint", "USERINFO", $userinfo);
 
             header('Content-Type: application/json; charset=utf-8');
